77e5760c12c00422ce1c980c7d862b26bf57ef35
Sergei Stadnik <sergeisqa@gmail.com>
2020-05-23 11:00:07 -0700

    Server Up and running

---
+++ chat/chat.go
@@ -0,0 +1,16 @@
+package chat
+
+import (
+	"log"
+
+	"golang.org/x/net/context"
+)
+
+type Server struct {
+}
+
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
+
+	log.Printf("Receive message body from client: %s", message.Body)
+	return &Message{Body: "Hello From the Server!"}, nil
+}
--- server.go
+++ server.go
@@ -3,9 +3,10 @@ package main
 import (
 	"log"
 	"net"
-    "google.golang.org/grpc"
-)
 
+	"github.com/monkrus/grpc-from0/chat"
+	"google.golang.org/grpc"
+)
 
 func main() {
 
@@ -14,7 +15,11 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	s := chat.Server{}
+
 	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
